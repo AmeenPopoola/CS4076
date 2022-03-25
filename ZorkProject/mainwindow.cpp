174ea08d9562efb3b85aec6333e06327e2619728
AmeenPopoola <20231717@studentmail.ul.ie>
2022-03-25 23:05:29 +0000

    GUI connected and minor changes to ZorkUL

--- ZorkProject/mainwindow.cpp
+++ ZorkProject/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap mapPic("C:/Users/ameen/Downloads/MapGUI.png");
+    int w = ui->map->width();
+    int h = ui->map->height();
+    ui->map->setPixmap(mapPic.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
@@ -36,4 +42,18 @@ void MainWindow::on_West_clicked()
 {
       ui->dirLabel->setText("WEST");
 }
+
+
+void MainWindow::on_quit_clicked()
+{
+    QMessageBox::StandardButton response = QMessageBox::question(this , "Quit" , "Do you want to quit the game?" ,
+                          QMessageBox::Yes | QMessageBox::No);
+    if(response == QMessageBox::Yes) {
+        QApplication::quit();
+    } else {
+        qDebug() << "No is clicked";
+    }
+
+    }
+
 
--- ZorkProject/mainwindow.h
+++ ZorkProject/mainwindow.h
@@ -31,6 +31,8 @@ private slots:
 
     void on_West_clicked();
 
+    void on_quit_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
